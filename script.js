d82692cfe2847827b2c14d4014433a07efc6d3d6
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-28 09:34:52 +0200

    Final project of sidebar navigation

---
+++ script.js
@@ -0,0 +1,13 @@
+// Chapter 3 - Exercises
+
+// 1: Minimum -  Define the function min that takes two arguments and returns their minimum
+
+// 2: Recursion -  Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
+
+// 3: Bean counting
+// a: Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase B characters there are in the string.
+
+// b: write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase B characters). Rewrite countBs to make use of this new function.
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+}
